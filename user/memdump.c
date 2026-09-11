8d5f8f6019b4fd8aa06dc1f367c7d4ded50af777
Alma Mashhood <a.mashhood.30529@khi.iba.edu.pk>
2026-09-11 18:36:17 +0500

    Implement memdump

--- user/memdump.c
+++ user/memdump.c
@@ -57,9 +57,90 @@ main(int argc, char *argv[])
   exit(0);
 }
 
+static void
+print_hex64(uint64 val)
+{
+  char digits[] = "0123456789ABCDEF";
+  char buf[17];
+  int i = 16;
+  buf[16] = '\0';
+  if (val == 0) {
+    printf("0\n");
+    return;
+  }
+  while (val > 0) {
+    i--;
+    buf[i] = digits[val & 0xf];
+    val >>= 4;
+  }
+  printf("%s\n", buf + i);
+}
+
 void
 memdump(char *fmt, char *data, int len)
 {
-  // Your code here.  `data` holds `len` valid bytes.
+  int pos = 0;
+  int i;
 
+  for (i = 0; fmt[i] != '\0'; i++) {
+    char f = fmt[i];
+
+    if (f == 'i') {
+      if (pos + 4 > len) {
+        printf("memdump: not enough data for '%c'\n", f);
+        return;
+      }
+      int val;
+      memmove(&val, data + pos, 4);
+      printf("%d\n", val);
+      pos += 4;
+
+    } else if (f == 'p') {
+      if (pos + 8 > len) {
+        printf("memdump: not enough data for '%c'\n", f);
+        return;
+      }
+      uint64 val;
+      memmove(&val, data + pos, 8);
+      print_hex64(val);
+      pos += 8;
+
+    } else if (f == 'h') {
+      if (pos + 2 > len) {
+        printf("memdump: not enough data for '%c'\n", f);
+        return;
+      }
+      short val;
+      memmove(&val, data + pos, 2);
+      printf("%d\n", val);
+      pos += 2;
+
+    } else if (f == 'c') {
+      if (pos + 1 > len) {
+        printf("memdump: not enough data for '%c'\n", f);
+        return;
+      }
+      printf("%c\n", data[pos]);
+      pos += 1;
+
+    } else if (f == 's') {
+      if (pos + 8 > len) {
+        printf("memdump: not enough data for '%c'\n", f);
+        return;
+      }
+      char *ptr;
+      memmove(&ptr, data + pos, 8);
+      printf("%s\n", ptr);
+      pos += 8;
+
+    } else if (f == 'S') {
+      int j = pos;
+      while (j < len && data[j] != '\0') {
+        printf("%c", data[j]);
+        j++;
+      }
+      printf("\n");
+      pos = len;
+    }
+  }
 }
