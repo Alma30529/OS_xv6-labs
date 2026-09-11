cc24034acf53566e5144e85673c57e8a3ef52ad1
Alma Mashhood <a.mashhood.30529@khi.iba.edu.pk>
2026-09-11 23:40:50 +0500

    assignment1: add regex support to find

--- user/find.c
+++ user/find.c
@@ -4,6 +4,10 @@
 #include "kernel/fs.h"
 #include "kernel/param.h"
 
+int match(char *, char *);
+int matchhere(char *, char *);
+int matchstar(int, char *, char *);
+
 char*
 fmtname(char *path)
 {
@@ -61,7 +65,7 @@ find(char *path, char *target, char *execargv[], int execargc)
 
   switch (st.type) {
   case T_FILE:
-    if (strcmp(fmtname(path), target) == 0) {
+    if (match(target, fmtname(path))) {
       if (execargc > 0)
         run_exec(execargv, execargc, path);
       else
@@ -90,7 +94,7 @@ find(char *path, char *target, char *execargv[], int execargc)
         continue;
       }
 
-      if (strcmp(de.name, target) == 0) {
+      if (match(target, de.name)) {
         if (execargc > 0)
           run_exec(execargv, execargc, buf);
         else
@@ -128,4 +132,42 @@ main(int argc, char *argv[])
 
   find(argv[1], argv[2], execargv, execargc);
   exit(0);
+}
+
+
+
+int
+match(char *re, char *text)
+{
+  if (re[0] == '^')
+    return matchhere(re + 1, text);
+  do {
+    if (matchhere(re, text))
+      return 1;
+  } while (*text++ != '\0');
+  return 0;
+}
+
+int
+matchhere(char *re, char *text)
+{
+  if (re[0] == '\0')
+    return 1;
+  if (re[1] == '*')
+    return matchstar(re[0], re + 2, text);
+  if (re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if (*text != '\0' && (re[0] == '.' || re[0] == *text))
+    return matchhere(re + 1, text + 1);
+  return 0;
+}
+
+int
+matchstar(int c, char *re, char *text)
+{
+  do {
+    if (matchhere(re, text))
+      return 1;
+  } while (*text != '\0' && (*text++ == c || c == '.'));
+  return 0;
 }
